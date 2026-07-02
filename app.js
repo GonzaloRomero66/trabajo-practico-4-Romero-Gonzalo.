0ebe09b5484f39cf5477dadb30e86be90cf1cfb6
GonzaloRomero66 <estudio.romero66@gmail.com>
2026-07-02 20:12:15 -0300

    se conecto movie.router.js con app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
-
+import { startDB } from "./src/config/database";
+import { Movie } from "./src/models/movie.model.js"; 
+import { movieRouter } from "./src/routes/movie.routes.js"; 
 const app = express();
 const PORT = 3000;
 app.use(express.json());
+app.use("/api", movieRouter)
 app.listen(PORT, async () => {
     await startDB();
-    console.log(`Servidor listo https://localhost:${PORT}`);
+    console.log(`Servidor listo http://localhost:${PORT}`);
 })
